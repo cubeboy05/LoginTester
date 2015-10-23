d2404c6a4adb256bad9a61b619f1592c36a6e514
cubeboy05 <cubeboy05@gmail.com>
2015-10-23 22:35:58 +0100

    New Test

---
+++ scripts.js
@@ -0,0 +1,29 @@
+	var isLogged = false;
+	/**
+	 * Method used to log into the application
+	 */
+
+	$(document).on("submit", "#form1", function(event) {
+
+	    alert("submit happened");
+	    event.preventDefault();
+	    $.ajax({
+	        type: "POST",
+	        url: "http://www.karsv.com/login.php",
+	        data: $(this).serialize(),
+	        error: function(jqXHR, textStatus, errorThrown) {
+	            alert(" server error " + textStatus + ":" + errorThrown)
+	        },
+	        success: function(data) {
+	            alert(data);
+	            if(data.match('successbully')) {
+	                isLogged = true;
+	                alert("JS - SUCCESS");
+	                $.mobile.changePage("#home");
+	            }
+	            else {
+	                alert("JS - You entered the wrong username or password. Please try again.");
+	            }
+	        }
+	    });
+	});
